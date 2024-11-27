3202e8fe5723b345a4b2de78214dc38639fc3892
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-11-27 23:00:51 +0500

    BROKERS: CRUD Product & Ogit

--- AgroCom/Brokers/Storages/StorageBroker.Ogit.cs
+++ AgroCom/Brokers/Storages/StorageBroker.Ogit.cs
@@ -6,5 +6,20 @@ namespace AgroCom.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Ogit> Ogits { get; set; }
+
+        public async ValueTask<Ogit> InsertOgitAsync(Ogit ogit) =>
+           await InsertAsync(ogit);
+
+        public async ValueTask<IQueryable<Ogit>> SelectAllOgitsAsync() =>
+           await SelectAllAsync<Ogit>();
+
+        public async ValueTask<Ogit> SelectOgitByIdAsync(int ogitId) =>
+            await SelectAsync<Ogit>(ogitId);
+
+        public async ValueTask<Ogit> UpdateOgitAsync(Ogit ogit) =>
+            await UpdateAsync(ogit);
+
+        public async ValueTask<Ogit> DeleteOgitAsync(Ogit ogit) =>
+            await DeleteAsync(ogit);
     }
 }
